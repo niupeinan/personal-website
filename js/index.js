93e45d858e30aade87c883bd795b462b5608b081
Your Name <867761374@qq.com>
2018-08-10 13:11:11 +0800

    3

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
-$(function(){
-	$(window).scroll(function(){
-		let top = $(this).scrollTop();
-		top>=30?$(".header").css("background","white"):$(".headr").css("background","");
-	})
-})
+window.onload = function(){
+	let header = document.querySelector(".header");
+	window.onscroll = function(){
+		let he = document.body.scrollTop || document.documentElement.scrollTop;
+		if(he >= 80){
+			header.style.background = "#fff";
+		}
+		else{
+			header.style.background = "rgba(255, 255, 255,0)";
+		}
+	}	
+}
